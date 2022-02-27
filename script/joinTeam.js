ea38bf9709fc664e5b9001db8d58f315d7838693
Samrat0to1 <samratkrishna345@gmail.com>
2022-02-27 14:43:22 +0530

    final commit

---
+++ script/joinTeam.js
@@ -0,0 +1,14 @@
+document.querySelector("#headBox>p:nth-child(1").addEventListener("click", function(){
+    window.location.href="about.html"
+})
+    document.querySelector("#headBox>p:nth-child(2").addEventListener("click", function(){
+    window.location.href="history.html"
+})
+
+    document.querySelector("#headBox>p:nth-child(3").addEventListener("click", function(){
+    window.location.href="joinTeam.html"
+})
+
+    document.querySelector("#headBox>p:nth-child(4").addEventListener("click", function(){
+    window.location.href="press.html"
+})
